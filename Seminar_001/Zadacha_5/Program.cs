6ebb8a4a5328a3835b25f033c86383adbb2b6eaa
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-01 23:11:15 +0700

    Add Zadacha_5

---
+++ Seminar_001/Zadacha_5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на входе принимает значение N, а на выходе показывает целые числа от -N до N
+Console.WriteLine("Введите число:");
+string nfirst = Console.ReadLine()??"";
+int n = Convert.ToInt32(nfirst);
+n = Math.Abs(n);
+for (int i = (-n); i <= n; i++)
+{
+ Console.Write(i+" ");  
+}
