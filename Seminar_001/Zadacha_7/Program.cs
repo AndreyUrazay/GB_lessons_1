7d8c0d8bbac81ee8bec09d1819b92ea4ea3f17d0
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-01 23:33:59 +0700

    Add Zadacha_7

---
+++ Seminar_001/Zadacha_7/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая принимает 3х значное число 
+и на выходе показывает последнее цифру этого числа
+456 - 6
+782 - 2
+918 -8
+*/
+
+Console.WriteLine ("Введите 3х значное число:");
+string nstring = Console.ReadLine()??"";
+int n = Convert.ToInt32(nstring);
+if ((n>=100) && (n<=999))
+    Console.WriteLine(n % 10);
+else
+    Console.WriteLine("Неверно введено число");
