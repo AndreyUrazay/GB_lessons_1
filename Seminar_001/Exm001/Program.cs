c7461ba18b3068868b4d93a8a6a8d84c973d60b9
Andrey Urazay <urazay.andrey@mail.ru>
2022-12-01 20:59:59 +0700

    Add Exm001 and Exm001_v2

---
+++ Seminar_001/Exm001/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+Напишите программу, которая на входе принимает число и выдает его квадрат (число умноженное на само себя).
+
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.WriteLine("Введите число:");
+string numberStr = Console.ReadLine() ??"";
+int number = Convert.ToInt32(numberStr);// -2147483647 to 2147483647  32bit
+// long q -9223372036854775807 to 9223372036854775807   64bit
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
